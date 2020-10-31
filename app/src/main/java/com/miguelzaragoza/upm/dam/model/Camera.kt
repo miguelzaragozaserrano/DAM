badc4ac4a902376730ddecca8915e4f5cecb4f05
mzaragoza <miguelzaragozaserrano@gmail.com>
2020-10-31 21:50:05 +0100

    Editing comment, adding Parcelize and adding class

--- app/src/main/java/com/miguelzaragoza/upm/dam/model/Camera.kt
+++ app/src/main/java/com/miguelzaragoza/upm/dam/model/Camera.kt
@@ -1,5 +1,8 @@
 package com.miguelzaragoza.upm.dam.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * Clase del objeto Camera.
  * Consta de:
@@ -8,10 +11,18 @@ package com.miguelzaragoza.upm.dam.model
  * 3.- Unas coordenadas de su ubicación.
  * 4.- Un estado que nos indica si es la cámara que
  *     está mostrándose actualmente (true) o no (false).
+ * @param name: nombre de la cámara
+ * @param url: enlace con el que se obtiene la captura de dicha cámara
+ * @param coordinates: coordenadas de donde se ubica la cámara
+ * @param status: valor que determina si tenemos la cámara seleccionada o no
  */
+@Parcelize
 data class Camera(
     val name: String,
     val url: String,
     val coordinates: String,
     var status: Boolean
-)
+): Parcelable
+
+@Parcelize
+class Cameras: ArrayList<Camera>(), Parcelable
