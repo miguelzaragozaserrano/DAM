211105d34b29dc6d52c1fdd5f4c0de3e6f2ffb77
mzaragoza <miguelzaragozaserrano@gmail.com>
2020-11-09 12:53:21 +0100

    Adding and editing comments

--- app/src/main/java/com/miguelzaragoza/upm/dam/binding/BindingAdapters.kt
+++ app/src/main/java/com/miguelzaragoza/upm/dam/binding/BindingAdapters.kt
@@ -30,9 +30,10 @@ import com.miguelzaragoza.upm.dam.modules.common.CamerasAdapter
  * 3.- Usamos la librería Glide para obtener la imagen de internet y cargarla en el ImageView.
  *     Durante la carga de la imagen añadimos una animación de carga y, en caso de que no se consiga
  *     cargar, mostramos una imagen como si el fichero estuviera roto.
- * @param imgView: vista del ImageView
- * @param imgUrl: enlace con la foto de la cámara que queremos mostrar
- * */
+ *
+ * @param imgView Vista del ImageView
+ * @param imgUrl Enlace con la foto de la cámara que queremos mostrar
+ */
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?){
     imgUrl?.let{
@@ -53,8 +54,9 @@ fun bindImage(imgView: ImageView, imgUrl: String?){
  * Para ello, le pasamos el View correspondiente a la función y la posible lista de objetos Cámara.
  * 1.- Asignamos el adaptador CamerasAdapter a una variable.
  * 2.- Le pasamos la lista que queremos que muestre.
- * @param recyclerView: vista del RecyclerView
- * @param cameras: lista de cámaras
+ *
+ * @param recyclerView Vista del RecyclerView
+ * @param cameras Lista de cámaras
  */
 @BindingAdapter("listCameras")
 fun bindRecyclerView(recyclerView: RecyclerView, cameras: List<Camera>?){
@@ -62,17 +64,29 @@ fun bindRecyclerView(recyclerView: RecyclerView, cameras: List<Camera>?){
     adapter.setData(cameras)
 }
 
+/**
+ * Función que inicia el Mapa.
+ * Para ello le pasamos el View correspondiente a la función y la posible lista de objetos Cámara.
+ * 1.- Creamos el MapView y le asignamos el tipo normal por defecto.
+ * 2.- Centramos la cámara en la seleccionada.
+ * 3.- Añadimos todos los marcadores al mapa.
+ *
+ * @param mapView Vista del MapView
+ * @param cameras Lista de cámaras
+ */
 @BindingAdapter("initMap")
 fun bindMapView(mapView: MapView, cameras: Cameras?){
     mapView.onCreate(Bundle())
     mapView.getMapAsync {
         mapView.onResume()
         it.mapType = GoogleMap.MAP_TYPE_NORMAL
-
-        val cameraPosition = CameraPosition.Builder().target(cameras?.get(0)?.coordinates).zoom(12F).build()
-        it.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
-
-        cameras?.map { camera ->
+        for(camera in cameras!!){
+            if(camera.status){
+                val cameraPosition = CameraPosition.Builder().target(camera.coordinates).zoom(12F).build()
+                it.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+            }
+        }
+        cameras.map { camera ->
             it.addMarker(
                     MarkerOptions()
                             .position(camera.coordinates)
