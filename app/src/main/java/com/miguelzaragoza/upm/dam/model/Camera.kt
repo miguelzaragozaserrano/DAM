2537e687e1e19c0c05d7c9084361789581a16c25
mzaragoza <miguelzaragozaserrano@gmail.com>
2020-11-14 18:42:33 +0100

    Editing object Camera

--- app/src/main/java/com/miguelzaragoza/upm/dam/model/Camera.kt
+++ app/src/main/java/com/miguelzaragoza/upm/dam/model/Camera.kt
@@ -1,23 +1,32 @@
 package com.miguelzaragoza.upm.dam.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.parcel.Parcelize
 
 /**
  * Clase del objeto Camera.
  *
+ * @param id Identificador de la cámara.
  * @param name Nombre de la cámara.
  * @param url Enlace con el que se obtiene la captura de dicha cámara.
- * @param coordinates Coordenadas de donde se ubica la cámara.
- * @param status Valor que determina si tenemos la cámara seleccionada (true) o no (false).
+ * @param latitude Latitud (coordenada) donde se localiza la cámara.
+ * @param longitude Longitud (coordenada) donde se localiza la cámara.
+ * @param selected Determina si tenemos la cámara seleccionada o no.
+ * @param fav Determina si la cámara pertenece a las favoritas o no.
  */
+@Entity(tableName = "camera_table")
 data class Camera(
+    @PrimaryKey
     val id: Int,
     val name: String,
     val url: String,
-    val coordinates: LatLng,
-    var status: Boolean
+    val latitude: Double,
+    val longitude: Double,
+    var selected: Boolean,
+    var fav: Boolean
 )
 
 /**
