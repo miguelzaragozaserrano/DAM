5f2e9a44d4b982dbe69d4c9d4077e7e5e811b848
mzaragoza <miguelzaragozaserrano@gmail.com>
2020-11-14 21:45:19 +0100

    Adding database

--- app/src/main/java/com/miguelzaragoza/upm/dam/viewmodel/LoadingViewModelFactory.kt
+++ app/src/main/java/com/miguelzaragoza/upm/dam/viewmodel/LoadingViewModelFactory.kt
@@ -3,17 +3,20 @@ package com.miguelzaragoza.upm.dam.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.miguelzaragoza.upm.dam.database.CameraDao
 import com.miguelzaragoza.upm.dam.modules.loading.LoadingViewModel
 
 /**
  * ViewModelFactory que permite crear un LoadingViewModel al que pasarle parámetros por constructor
  */
-class LoadingViewModelFactory (private val application: Application
+class LoadingViewModelFactory (
+        private val application: Application,
+        private val database: CameraDao
 ) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(LoadingViewModel::class.java)) {
-            return LoadingViewModel(application) as T
+            return LoadingViewModel(application, database) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
