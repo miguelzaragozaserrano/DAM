817b00ba396ccaf7e0d6d3cc01a3bea60b52935c
mzaragoza <miguelzaragozaserrano@gmail.com>
2020-11-14 19:12:04 +0100

    Initial commit

---
+++ app/src/main/java/com/miguelzaragoza/upm/dam/model/MyCluster.kt
@@ -0,0 +1,46 @@
+package com.miguelzaragoza.upm.dam.model
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterItem
+
+/**
+ * Clase del objeto MyCluster
+ *
+ * @param position Posición que va a ocupar el marcador (cámara) en el mapa.
+ * @param title Título que le asignamos al marcador (cámara) en el mapa.
+ * @param snippet Snippet que le asignamos al marcador (cámara) en el mapa.
+ */
+class MyCluster(
+    private val position: LatLng,
+    private val title: String,
+    private val snippet: String
+) : ClusterItem {
+
+    /**
+     * Función que devuelve la posición.
+     *
+     * @return Objeto LatLng con la posición del marcador.
+     */
+    override fun getPosition(): LatLng {
+        return position
+    }
+
+    /**
+     * Función que devuelve el título.
+     *
+     * @return Título del marcador.
+     */
+    override fun getTitle(): String? {
+        return title
+    }
+
+    /**
+     * Función que devuelve el snippet.
+     *
+     * @return Snippet del marcador.
+     */
+    override fun getSnippet(): String? {
+        return snippet
+    }
+
+}
