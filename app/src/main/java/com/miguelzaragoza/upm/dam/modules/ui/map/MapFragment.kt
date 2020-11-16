c7e2bde81503aa78fcd699d0ce653cc264feff99
mzaragoza <miguelzaragozaserrano@gmail.com>
2020-11-16 13:30:02 +0100

    Adding polyline

---
+++ app/src/main/java/com/miguelzaragoza/upm/dam/modules/ui/map/MapFragment.kt
@@ -0,0 +1,149 @@
+package com.miguelzaragoza.upm.dam.modules.ui.map
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.miguelzaragoza.upm.dam.databinding.FragmentMapBinding
+import com.miguelzaragoza.upm.dam.modules.ui.map.MapViewModel.Companion.REQUEST_CODE
+import com.miguelzaragoza.upm.dam.viewmodel.MapViewModelFactory
+
+
+/**
+ * Fragment que muestra la tercera pantalla
+ * donde aparece el mapa con las cámaras.
+ */
+class MapFragment : Fragment() {
+
+    /******************************** VARIABLES BÁSICAS ********************************
+     ***********************************************************************************/
+
+    /**
+     * Inicializamos con lazy nuestro [MapViewModel] para crearlo con un método
+     * lifecycle apropiado.
+     */
+    private val mapViewModel: MapViewModel by lazy{
+        val application = requireNotNull(this.activity).application
+        ViewModelProvider(this, MapViewModelFactory(application))
+                .get(MapViewModel::class.java)
+    }
+
+    /******************************* FUNCIONES OVERRIDE *******************************
+     **********************************************************************************/
+
+    /**
+     * Función que se llama para instanciar la vista de interfaz de usuario (UI).
+     *
+     * @param inflater El objeto LayoutInflater se usa para inflar cualquier vista
+     * en el Fragment.
+     * @param container Si no es nulo, esta es la vista principal a la que se debe adjuntar
+     * la UI del Fragment. El Fragment no debe agregar la vista en sí, pero esto puede
+     * usarse para generar los LayoutParams de la vista.
+     * @param savedInstanceState Si no es nulo, este Fragment se está reconstruyendo a partir
+     * de un estado guardado anterior como se indica aquí.
+     *
+     * @return Devuelve la vista UI del Fragment.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentMapBinding.inflate(inflater)
+
+        /* Permite a Data Binding observar LiveData con el lifecycle de su Fragment */
+        binding.lifecycleOwner = this
+
+        /* Recogemos la lista de cámaras que se pasa entre fragmentos y si queremos un
+        *  activar el modo cluster o no */
+        val cameras = MapFragmentArgs.fromBundle(requireArguments()).cameras
+        val cluster = MapFragmentArgs.fromBundle(requireArguments()).cluster
+
+        /* Guardamos la cámara seleccionada, para poder centrar el mapa */
+        if(mapViewModel.camera == null){
+            for(camera in cameras){
+                if(camera.selected) mapViewModel.camera = camera
+            }
+        }
+
+        /* Damos acceso binding a MapViewModel, al cluster y a las cámaras */
+        binding.cameras = cameras
+        binding.cluster = cluster
+        binding.viewModel = mapViewModel
+
+        /* Declaramos un iconLocation y googleMap en el ViewModel
+        *  para poder actuar con ellos mediante la UI */
+        mapViewModel.iconLocation = binding.iconLocation
+        binding.mapView.getMapAsync {
+            mapViewModel.googleMap = it
+        }
+
+        /* Dependiendo del chip seleccionado, se mostrará un tipo de mapa u otro */
+        binding.chipGroup.setOnCheckedChangeListener { _, checkedId ->
+            when(checkedId){
+                binding.hybridChip.id -> mapViewModel.changeTypeMap(GoogleMap.MAP_TYPE_HYBRID)
+                binding.satelliteChip.id -> mapViewModel.changeTypeMap(GoogleMap.MAP_TYPE_SATELLITE)
+                binding.normalChip.id -> mapViewModel.changeTypeMap(GoogleMap.MAP_TYPE_NORMAL)
+            }
+        }
+
+        /* Observamos la variable askPermissions. Si toma un valor distinto de null,
+        *  es debido a que se ha pulsado el icono de localización y queremos activar nuestra
+        *  ubicación actual */
+        mapViewModel.askPermissions.observe(viewLifecycleOwner, {
+            if (it != null)
+                requestPermissions(
+                    arrayOf(
+                        mapViewModel.fineLocationPermission
+                    ), REQUEST_CODE
+                )
+        })
+
+        return binding.root
+    }
+
+    /**
+     * Función que se llama para solicitar los permisos.
+     *
+     * @param requestCode Código de petición.
+     * @param permissions Lista de permisos.
+     * @param grantResults Permisos concedidos.
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> {
+                /* En caso de que se haya concedido los permisos, calculamos la ruta hasta la
+                *  cámara seleccionada desde nuestra ubicación */
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    mapViewModel.mLocationPermissionGranted = true
+                    mapViewModel.getLocation()
+                }
+            }
+        }
+        /* Marcamos nuestra ubicación en el mapa */
+        mapViewModel.updateLocationUI()
+    }
+
+    /**
+     * Función que desactiva la ubicación del dispositivo cuando se destruye el Fragment.
+     */
+    @SuppressLint("MissingPermission")
+    override fun onDestroy() {
+        super.onDestroy()
+        if(mapViewModel.googleMap.isMyLocationEnabled){
+            mapViewModel.googleMap.isMyLocationEnabled = false
+            mapViewModel.googleMap.uiSettings.isMyLocationButtonEnabled = false
+        }
+    }
+
+}
---
+++ app/src/main/java/com/miguelzaragoza/upm/dam/modules/ui/map/MapViewModel.kt
@@ -0,0 +1,238 @@
+package com.miguelzaragoza.upm.dam.modules.ui.map
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.pm.PackageManager
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.gson.Gson
+import com.miguelzaragoza.upm.dam.R
+import com.miguelzaragoza.upm.dam.model.Camera
+import com.miguelzaragoza.upm.dam.model.Response
+import com.miguelzaragoza.upm.dam.modules.utils.decodePoly
+import com.miguelzaragoza.upm.dam.modules.utils.hasConnection
+
+/**
+ * ViewModel que realizará las funciones lógicas y almacenará los datos del
+ * MapFragment.
+ *
+ * @param application: objeto Application que nos permitirá obtener el contexto de la aplicación
+ */
+class MapViewModel(application: Application): AndroidViewModel(application) {
+
+    /******************************** VARIABLES BÁSICAS ********************************
+     ***********************************************************************************/
+
+    /* Variables privadas para definir el contexto cuando sea necesario */
+    private val context = application.applicationContext
+
+    /* Variable que almacena el objeto Polyline con la ruta */
+    private lateinit var polyline: Polyline
+    /* Variable que almacena la ubicación del dispositivo */
+    private lateinit var myLocation: LatLng
+    /* Variable que nos permite obtener la última ubicación del dispositivo */
+    @SuppressLint("VisibleForTests")
+    private var fusedLocationClient: FusedLocationProviderClient = FusedLocationProviderClient(context)
+
+    /* Variable que almacena la cámara seleccionada */
+    var camera: Camera? = null
+    /* Variable que nos permite configurar el mapa */
+    lateinit var googleMap: GoogleMap
+    /* Variable que almacena el icono de la ubicación */
+    lateinit var iconLocation: ImageView
+    /* Variable que determina si tenemos permisos o no */
+    var mLocationPermissionGranted = false
+    /* Variable que almacena el permiso de la ubicación precisa */
+    val fineLocationPermission = android.Manifest.permission.ACCESS_FINE_LOCATION
+
+    /****************************** VARIABLE ENCAPSULADA ******************************
+     **********************************************************************************/
+
+    /* Variable para controlar la solicitud de permisos */
+    private val _askPermissions = MutableLiveData<Boolean>()
+    val askPermissions: LiveData<Boolean>
+        get() = _askPermissions
+
+    /********************************* COMPANION OBJECT ********************************
+     **********************************************************************************/
+
+    companion object{
+        const val REQUEST_CODE = 100
+    }
+
+    /********************************* BLOQUE INICIAL *********************************
+     **********************************************************************************/
+
+    init {
+        try{
+            MapsInitializer.initialize(context)
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+
+    /******************************** FUNCIONES BÁSICAS ********************************
+     ***********************************************************************************/
+
+    /**
+     * Función que cambia el tipo de Mapa.
+     *
+     * @param type Variable que contiene el tipo de mapa.
+     */
+    fun changeTypeMap(type: Int){
+        googleMap.mapType = type
+    }
+
+    /***************************** FUNCIONES LOCALIZACIÓN ******************************
+     ***********************************************************************************/
+
+    /**
+     * Función que se activa cuando se pulsa el botón de la ubicación. Actua dependiendo de si
+     * estaba activo o no.
+     */
+    @SuppressLint("MissingPermission")
+    fun setLocation(){
+        /* En caso de no estar activo, comprobamos si hay conexión */
+        if(!googleMap.isMyLocationEnabled){
+            if(hasConnection(getApplication<Application>())){
+                /* Si hay conexión, obtenemos los permisos y cambiamos el icono */
+                getLocationPermission()
+                iconLocation.setImageResource(R.drawable.ic_marker_on)
+            }else
+                /* Si no hay conexión, mostramos un mensaje */
+                Toast
+                    .makeText(context, context.getString(R.string.check_connection), Toast.LENGTH_LONG)
+                    .show()
+
+        }else{
+            /* Si estaba activo, lo desactivamos, eliminamos la ruta y cambiamos el icono */
+            googleMap.isMyLocationEnabled = false
+            googleMap.uiSettings.isMyLocationButtonEnabled = false
+            polyline.remove()
+            iconLocation.setImageResource(R.drawable.ic_marker_off)
+        }
+    }
+
+    /**
+     * Función que nos devuelve la localización en caso de que ya tengamos los permisos aceptados,
+     * o que pregunta en caso de no tenerlos.
+     */
+    private fun getLocationPermission(){
+        if (ActivityCompat
+                        .checkSelfPermission(
+                                context, fineLocationPermission
+                        ) == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true
+            getLocation()
+        }else{
+            askPermissions()
+        }
+    }
+
+    /**
+     * Función que nos devuelve la localización actual del dispositivo.
+     */
+    @SuppressLint("MissingPermission")
+    fun getLocation(){
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            myLocation = LatLng(it.latitude, it.longitude)
+            googleMap.isMyLocationEnabled = true
+            googleMap.uiSettings.isMyLocationButtonEnabled = true
+            /* Creamos un String para hacer la solicitud a la API de Google Maps */
+            val origin = "origin=" + myLocation.latitude + "," + myLocation.longitude + "&"
+            val destination = "destination=" +
+                    camera?.latitude +
+                    "," + camera?.longitude + "&"
+            val parameters = origin +
+                    destination +
+                    "sensor=false&mode=driving&key=" +
+                    getApplication<Application>().getString(R.string.google_maps_key)
+            loadURL("https://maps.googleapis.com/maps/api/directions/json?$parameters")
+        }
+
+    }
+
+    /**
+     * Función que hace la llamada a la API de Google Maps.
+     *
+     * @param url Url que queremos cargar.
+     */
+    private fun loadURL(url: String){
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(Request.Method.GET, url, { response ->
+            /* Con la respuesta de la API, obtenemos las coordenadas */
+            val coordinates = getCoordinates(response)
+            /* Añadimos las coordenadas al Mapa */
+            polyline = googleMap.addPolyline(coordinates)
+        }, {})
+        queue.add(request)
+    }
+
+    /**
+     * Función que obtiene las coordenadas para mostrar la ruta hasta la cámara a través
+     * de la respuesta de la API.
+     *
+     * @param response Respuesta de la API.
+     * @return Las coordenadas del Polyline para añadir al mapa.
+     */
+    private fun getCoordinates(response: String): PolylineOptions{
+        /* Utilizamos la clase Response para obtener los datos */
+        val obj = Gson().fromJson(response, Response::class.java)
+        val steps = obj.routes?.get(0)!!.legs?.get(0)!!.steps
+        val coordinates = PolylineOptions()
+        for(step in steps!!){
+            /* Decodificamos las coordenadas del Polyline */
+            decodePoly(step.polyline?.points.toString(), coordinates)
+        }
+        /* Le asignamos un color a la ruta */
+        coordinates.color(getApplication<Application>().getColor(R.color.indigo_900_dark)).width(15f)
+        return coordinates
+    }
+
+    /**
+     * Función que activa el proceso solicitar permisos.
+     */
+    private fun askPermissions(){
+        _askPermissions.value = true
+        askPermissionsComplete()
+    }
+
+    /**
+     * Función que certifica que el proceso de solicitar permisos se ha completado.
+     */
+    private fun askPermissionsComplete(){
+        _askPermissions.value = null
+    }
+
+    /**
+     * Función que actualiza la localización del dispositivo.
+     */
+    fun updateLocationUI() {
+        try {
+            if (mLocationPermissionGranted) {
+                googleMap.isMyLocationEnabled = true
+                googleMap.uiSettings.isMyLocationButtonEnabled = true
+            } else {
+                googleMap.isMyLocationEnabled = false
+                googleMap.uiSettings.isMyLocationButtonEnabled = false
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+}
